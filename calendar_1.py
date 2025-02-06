063668737210a20b05fb037270a7107f955488e9
CoffeePuma55644 <coffeepuma55644@gmail.com>
2025-02-06 09:50:20 +0100

    ajout

---
+++ calendar_1.py
@@ -0,0 +1,5 @@
+liste_de_course_prix = [10,20,88,450,144] 
+
+reduction = [item/2 for item in liste_de_course_prix]
+
+print(reduction)
---
+++ prog.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int pause(int rompish)
+{
+
+    rompish = 47;
+
+    return rompish;
+}
+
+int actif(int travail)
+{
+    int boss = 8;
+    return boss;
+}
+
+int main(void)
+{
+
+    int nombre_de_pose = pause(nombre_de_pose);
+
+    printf("Le nombre de pose est %d\n", nombre_de_pose);
+
+    int nombre_de_boss = actif(nombre_de_boss);
+
+    printf("Le nombre de boss est %d\n", nombre_de_boss);
+
+    return 0;
+}
